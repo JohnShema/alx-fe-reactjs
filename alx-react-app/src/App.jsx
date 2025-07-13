4a9f3e1ee4ca4fdb3526129bc6d8b0e4553ca9e6
Shema John <johnshema7@gmail.com>
2025-07-13 22:20:15 +0200

    staged package-lock.json

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile.jsx';
 import WelcomeMessage from './components/WelcomeMessage';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
@@ -37,6 +38,11 @@ function App() {
       </div>
      <Header />
      <MainContent />
+    <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
      <Footer /> 
     </>
   )
